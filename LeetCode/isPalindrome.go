8dce55888f8d8f1aa9b102c4e0c1402e4fa9f4bf
Putra Fajar <putrautalls@gmail.com>
2022-08-30 23:53:08 +0700

    is palindrome, leetcode

---
+++ LeetCode/isPalindrome.go
@@ -0,0 +1,16 @@
+package main
+
+func isPalindrome(x int) bool {
+	var sum int = 0
+	var compare = x
+	for x > 0 {
+		r := x % 10
+		sum = (sum * 10) + r
+		x = x / 10
+
+	}
+	if compare == sum {
+		return true
+	}
+	return false
+}
