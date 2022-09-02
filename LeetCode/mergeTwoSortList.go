8f4d5b1e5f0badfb95c177516d8bc9ae1a8c300a
Putra Fajar <putrautalls@gmail.com>
2022-09-02 23:14:04 +0700

    merge two sorted lists, leetcode

---
+++ LeetCode/mergeTwoSortList.go
@@ -0,0 +1,23 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	} else if list2 == nil {
+		return list1
+	}
+	var head *ListNode
+	if list1.Val < list2.Val {
+		head = list1
+		head.Next = mergeTwoLists(list1.Next, list2)
+	} else {
+		head = list2
+		head.Next = mergeTwoLists(list2.Next, list1)
+	}
+	return head
+}
